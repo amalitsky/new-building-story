ada2a493d8b27810e28b136814fcdaf48fc06f33
Malitsky Alexander <a.malitsky@gmail.com>
2014-02-14 17:25:48 -0800

    serverside got email alerts, log files and schemas dumps, alternative table backup of all parsed from web flats, export to json and json.gz

--- crawler.php
+++ crawler.php
@@ -1,94 +1,242 @@
 ﻿<!DOCTYPE html>
-<html>
-<head>
-<title>парсер шахматки</title>
+<html><head>
+<title>new buildings appartments parser</title>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-</head>
-<pre>
-<?
-error_reporting(E_NOTICE);
-require ("./simple_html_dom.php");
-$db = new mysqli("localhost", 'root', '', 'bcrawler');
-if ($db->connect_errno) { echo "Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error; }
-
-/*function countIdFromNeighbour($list, $flat2countId){
-	$res = NULL;
-	$len = count($list);
-	for($i = 0; $i < $len; $i++){
-		if($list[$i][0] !== 0){
-			$res  = $list[$i][0] - ($list[$i][4] - $list[$flat2countId][4]);
-			break;
-			}
-		};
-	if($i === $len){
-		echo "countIdFromNeighbour ERROR: Can't find an id for a fixed flat - no relevant flat IDs are present in array."; }
-	return $res;
-	}*/
-
-#$needle = '~<td class="free buildroom room_[123] set_([^"]*)"><a href="\?id=([0-9]{6})&build=(1709|1708|1710)">[^<]*</a></td>~i';
-
-//flat status: 0 - never available, 1 - available(on Sale), 2 - fixed, 3 - sold out
-$html = file_get_html('./examples/k1example_06022014.htm');
-$tdElems = $html -> find('table.housemodbigs table.housemod td.buildroom');
-$parsedFlats = 0; $freshOnSale = array (), $allOnSale = array();
-foreach($tdElems as $key => $tdElem) {
-	if( preg_match("~free.*?_([[:digit:]]{7,8})\">(<a href=\"\?id=([[:digit:]]{6})&build=(1708|1709|1710)\">.*)?[^<]*</td>~i", $tdElem->outertext, $matches)){
-		switch (intval($matches[4])){
-			case 1708: $bId = 1; break;
-			case 1709: $bId = 2; break;
-			case 1710: $bId = 3; break;
-			//default:
-				}
-		if(!($stmt = $db->prepare("SELECT flStatus, flPrice FROM `snapshots` WHERE bId=? AND extFlatId=? ORDER BY snapDate LIMIT 1;"))){
-			echo "Prepare failed: (" . $stmt->errno . ") " . $stmt->error; }
-		if(!$stmt->bind_param("ii", $bId, intval($matches[3]))) { echo "Binding parameters failed: (" . $stmt->errno . ") " . $stmt->error; }
-		//echo($bId.", ".intval($matches[3)."<br>");
-		if (!$stmt->execute()) { echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error; }
-		$lastflStatus = NULL;
-		$lastflPrice = NULL;
-		if (!$stmt->bind_result($lastflStatus, $lastflPrice)) { echo "Binding output parameters failed: (" . $stmt->errno . ") " . $stmt->error; }
-		$stmt->fetch();
-		//echo ('Достаём данные по квартире "'.intval($matches[3]).'": последний статуc: "'.$lastflStatus.'", последняя цена: "'.$lastflPrice.'".<br>');
-		$stmt->close();
-		$tmp[2] = intval($bId);//building
-		$tmp[4] = intval($matches[3]);//extId
-		$tmp[5] = 1;//status onSale
-		$tmp[6] = intval($matches[1]);//price
-		if($lastflStatus != $tmp[5] OR $lastflPrice != $tmp[6]) { $freshOnSale[] = $tmp; }
-		else { $allOnSale[] = $tmp; }
-		$tmp = array();
-		$parsedFlats++;
-		}
+<style>
+body{
+	font-family:Arial;
+	font-size:12px;
 	}
-	unset($tdElem);
-
-	# take all flats what were on sale with this bId for the last time and find ones, not presented in a list
+div.scriptExecTime{	
+	position:fixed;
+	color:red;
+	padding:0.2em 0.3em;
+	top:1em;right:1em;
+	background-color:white;
+	border:1px solid grey
+	}
 	
-	$upd_stmt  = $db->prepare("INSERT INTO snapshots(bId, extFlatId, flStatus, flPrice) VALUES (?,?,?,?);");
-	foreach ($list as $flat){
-		//echo("Пробуем записать в базу bId='".$flat[2]."', extFlatId='".$flat[4]."', flStatus='".$flat[5]."', fPrice='".$flat[6]."'.<br>");
-		$upd_stmt ->bind_param('iiii', $flat[2], $flat[4], $flat[5], $flat[6]);
-		if (!$upd_stmt->execute()) { echo "Execute failed: (" . $upd_stmt->errno . ") " . $upd_stmt->error; }
+p.upd, pre{
+	font-size:12px;
+	background-color:#eee;
+	border:1px solid #ccc
+	}
+</style>
+</head><body><?
+$currHour = gmdate("G"); $currMinute = gmdate("m");
+//if($currHour >= 21 OR $currHour < 03) { exit('MSS'); } //Moscow sometimes sleep, let's stop bothering
+//usleep (mt_rand(0,600)*1000000);  //we don't need english courtesy to be just in time
+$time_start = microtime(true);
+error_reporting(E_NOTICE);
+gc_disable();
+ob_start();
+require ("./simple_html_dom.php"); 
+function compareFlatIds ($a, $b){ return $a[4] - $b[4]; }
+function compareFlats ($a, $b) { return strcmp(implode("-", $a), implode("-", $b) ); }
+function compareFlatsByIdAndBId($a, $b) {
+	if($a[2] > $b[2]){ $res = 1;}
+	else if ($a[2] < $b[2]) { $res = -1;}
+	else {
+		if ($a[4] > $b[4]) { $res = 1;}
+		else if ($a[4] < $b[4]) { $res = -1;}
+		else { $res = 0; }
 		}
-$upd_stmt->close();
-echo("Flats parsed: ".$parsedFlats.", saved to db: ".count($list).".");
-$db->close();
+	return $res;
+	} 
+function parseRmk9($tableObj, $building){
+	$tdElems = $tableObj -> find('table.housemod td.buildroom'); 
+	if(!count($tdElems)){
+		echo "<p>".gmdate("Y-m-d H:i:s T")." Error: File on address '".$building[1]."' for building '".$building[0]."' can't be parsed.</p>\r\n";
+		return false;
+		}	
+	$flatsOnSale = array();
+	foreach($tdElems as $key => $tdElem) {
+		if( preg_match("~free.*?_([[:digit:]]{7,8})\">(<a href=\"\?id=([[:digit:]]{6})&build=(1708|1709|1710)\">.*)?[^<]*</td>~i", $tdElem->outertext, $matches)){
+			$tmp = array();
+			$tmp[2] = intval($building[0]);//internal building ID
+			$tmp[4] = intval($matches[3]);//extId
+			$tmp[5] = 1;//status 'onSale'(available), only this is applicapable
+			$tmp[6] = intval($matches[1]);//price
+			$flatsOnSale[] = $tmp;
+			}
+		}
+	return $flatsOnSale;
+	}
 
-/*foreach ($list as $key => &$flat){
-	if($flat[0] === 0){ //id is not provided when flat is "fixed"
-		$flat[0] = countIdFromNeighbour($list, $key);
-		}
-	unset($flat);
-	}*/
-//echo var_dump($list);
-class snapshotExp {
+function saveLog($text){
+	$text = "<div class='logEntity'>\r\n".$text."</div>\r\n";
+	$i = 0;
+	while(file_exists('./logs/log'.$i.'.html') && filesize('./logs/log'.$i.'.html') > 5242880){ $i++; }
+	//if(!(file_exists($fileName)) || filesize($fileName) < 5242880) { break; }
+	if(!file_put_contents('./logs/log'.$i.'.html', $text, FILE_APPEND | LOCK_EX)){
+		echo "<p>Error: Can\'t save ouput to log file.</p>\r\n"; }
+	};
+	
+function exportflats2js($bId){
+	
+	/*class snapshotExp {
 	public $date, $list;
 	function __construct($list) {
 		$this->date = time();
 		$this->list = $list;
 		}
 	}
+	echo json_encode(new snapshotExp($list));*/
+	};
+	
+$buildings = array( array (1, 'http://novokosino.ndv.ru/sale/?build=1708'), array (2, 'http://novokosino.ndv.ru/sale/?build=1709'));
+	
+$buildings = array(
+	//array (1, './examples/k1example_05022014.htm'),
+	//array (1, './examples/k1example_06022014.htm'),
+	//array (1, './examples/k1example_07022014.htm'),
+	//array (1, './examples/k1example_08022014.htm'),
+	//array (1, './examples/k1example_10022014.htm'),
+	//array (1, './examples/k1example_11022014.htm'),
+	//array (1, './examples/k1example_12022014.htm'),
+	array (1, './examples/k1example_13022014.htm'),
+	//array (1, './examples/k1example_14022014.htm'),
+	//array (1, 'http://novokosino.ndv.ru/sale/?build=1708'),
+	//array (2, './examples/k2example_05022014.htm'),
+	//array (2, './examples/k2example_06022014.htm'),
+	//array (2, './examples/k2example_07022014.htm'),
+	//array (2, './examples/k2example_08022014.htm'),
+	//array (2, './examples/k2example_10022014.htm'),
+	//array (2, './examples/k2example_11022014.htm'),
+	//array (2, './examples/k2example_12022014.htm'),
+	array (2, './examples/k2example_13022014.htm'),
+	//array (2, './examples/k2example_14022014.htm'),
+	//array (2, 'http://novokosino.ndv.ru/sale/?build=1709'),
+	//array (3, 'http://novokosino.ndv.ru/sale/?build=1710')
+	);
+	
+$i = 0;
+$soldFlats = array();
+$newFlatsOnSale = array();
+$flatsPricesUpd = array();
+$updArr = array();
+$backupArr = array();
+$db = new mysqli("localhost", 'root', '', 'bcrawler');
+if ($db->connect_errno) {
+	echo "<p>Error: Failed to connect to MySQL: (" . $db->connect_errno . ") " . $db->connect_error ."</p>\r\n"; }
 
-//echo json_encode(new snapshotExp($list));
-?>
-</pre></html>
+//flat status: 0 - never available, 1 - available(on Sale), 2 - fixed, 3 - sold out
+if(!($selectStmt = $db->prepare("SELECT extFlatId, flStatus, flPrice FROM `snapshots` WHERE snapId IN (SELECT MAX(snapId) FROM `snapshots` WHERE bId=? GROUP BY extFlatId) AND flStatus='1';"))) {
+	echo "<p>Error: selectStmt prepare failed: (" . $selectStmt->errno . ") " . $selectStmt->error ."<p>\r\n";
+	}
+
+foreach ($buildings as $building){//parsing, saving copy and counting differences between snapshots
+	if(!($file = file_get_contents($building[1]))){
+		echo "<p>".gmdate("Y-m-d H:i:s T")." Error: File on address '".$building[1]."' for building '".$building[0]."' can't be even loaded.</p>\r\n";
+		continue;
+		};
+	$html = str_get_html($file);
+	$table = $html -> find('table.housemodbigs', 0);
+	$res[$i] = parseRmk9($table, $building);
+	$table = $table -> outertext;
+	if(!$res[$i]) { continue; }
+	if(!$selectStmt->bind_param('i', $building[0])) {	
+		echo "<p>Error: selectStmt binding parameters failed: (" . $selectStmt->errno . ") " . $selectStmt->error ."</p>\r\n"; }
+	if(!$selectStmt->execute()) { echo "<p>Error: selectStmt execute failed: (" . $selectStmt->errno . ") " . $selectStmt->error ."</p>\r\n"; }
+	if (!($fromdb[$i] = $selectStmt->get_result())) {
+		echo "<p>Error: selectStmt getting result set failed: (" . $selectStmt->errno . ") " . $selectStmt->error ."</p>\r\n"; }
+	$fromdb[$i] = $fromdb[$i]->fetch_all();
+	foreach($fromdb[$i] as &$flat){//change flat format from db style to out-of-parser, needed for compareFlatPrices
+		$flat[4] = $flat[0];
+		$flat[5] = $flat[1];
+		$flat[6] = $flat[2];
+		$flat[2] = $building[0];
+		unset($flat[0], $flat[1]);
+		}
+	unset($flat);
+	$soldFlats = array_udiff($fromdb[$i], $res[$i], 'compareFlatIds'); //need only onSale status form DB
+	foreach($soldFlats as &$flat){ $flat[5] = 3; } //change status to 'sold'
+	$newFlatsOnSale = array_udiff($res[$i], $fromdb[$i], 'compareFlatIds'); //need only onSale status from DB
+	$flatsOnSaleFromPrevCheck = array_udiff($res[$i], $newFlatsOnSale, 'compareFlatIds'); 
+	$flatsPricesUpd = array_udiff($flatsOnSaleFromPrevCheck, $fromdb[$i], 'compareFlats');
+	$updArr = array_merge($updArr, $soldFlats, $flatsPricesUpd, $newFlatsOnSale);
+	$backupArr = array_merge($backupArr, $res[$i]);
+	$soldQ = count($soldFlats);
+	$newFlatsOnSaleQ = count($newFlatsOnSale);
+	$flatsPricesUpdQ = count($flatsPricesUpd);
+	/*echo '<pre>';
+	if($soldQ) { echo "sold: <br>"; print_r($soldFlats); }
+	if($newFlatsOnSaleQ) { echo "new on sale: <br>"; print_r($newFlatsOnSale); }
+	if($flatsPricesUpdQ) { echo "changed price: <br>"; print_r($flatsPricesUpd); }
+	if(count($updArr)) { echo "whole update array:<br>"; print_r($updArr); }
+	echo '</pre>';*/
+	
+	if($soldQ || $newFlatsOnSaleQ || $flatsPricesUpdQ){
+		if(file_put_contents("compress.zlib://./dumps/".$building[0]."/".gmdate("Ymd_His").".html.gz", $table) === 0) {
+			echo "<p>Error: File dump for building '".$building[0]."' wasn't saved.</p>\r\n"; }
+		}
+	
+	echo "<p class='result'>".gmdate("Y-m-d H:i:s T ")."<b>".count($res[$i])."</b> flats were found by parser flats on page '".$building[1]."' for building ".$building[0].".<br>\r\n ".count($fromdb[$i])." flats were loaded from last shapshot. <b>".($soldQ+$newFlatsOnSaleQ+$flatsPricesUpdQ)."</b> records will be saved: ".$soldQ." sold, ".$newFlatsOnSaleQ." put up on sale, ".$flatsPricesUpdQ." price changed.</p>\r\n\r\n";
+	$i++;
+	}
+$selectStmt->close();
+
+if(count($updArr)){//save all updated flats to db
+	usort($updArr,'compareFlatsByidAndBId');
+	if(!($updStmt  = $db->prepare("INSERT INTO snapshots(bId, extFlatId, flStatus, flPrice) VALUES (?,?,?,?);"))){
+		echo "<p>Error: updStmt prepare failed: (" . $updStmt->errno . ") " . $updStmt->error ."</p>\r\n"; }
+	foreach ($updArr as &$flat){
+		//echo("<p>Trying to save in database entity: bId='".$flat[2]."', extFlatId='".$flat[4]."', flStatus='".$flat[5]."', fPrice='".$flat[6]."'.</p>");
+		$updStmt ->bind_param('iiii', $flat[2], $flat[4], $flat[5], $flat[6]);
+		if (!$updStmt->execute()) { echo "<p>Error: updStmt execute failed: (" . $updStmt->errno . ") " . $updStmt->error. "</p>\r\n"; }
+		}
+	unset($flat);
+	$updStmt->close();
+	};
+
+if(count($updArr) && count($backupArr)){//makes a copy of all flats onSale presented on a web site to an extra table
+	usort($backupArr,'compareFlatsByidAndBId');
+	if(!($updStmt  = $db->prepare("INSERT INTO snapbackup(bId, extFlatId, flStatus, flPrice) VALUES (?,?,?,?);"))){
+		echo "<p>Error: updStmt prepare failed: (" . $updStmt->errno . ") " . $updStmt->error. "</p>\r\n"; }
+	foreach ($backupArr as &$flat){
+		//echo("<p>Going to save in backup database entity: bId='".$flat[2]."', extFlatId='".$flat[4]."', flStatus='".$flat[5]."', fPrice='".$flat[6]."'.</p>");
+		$updStmt ->bind_param('iiii', $flat[2], $flat[4], $flat[5], $flat[6]);
+		if (!$updStmt->execute()) { echo "<p>Error: updStmt execute failed: (" . $updStmt->errno . ") " . $updStmt->error ."</p>\r\n"; }
+		}
+	unset($flat);
+	$updStmt->close();
+	};
+	
+if(1 || count($updArr)){//export database to JSON
+	if(!($expStmt  = $db->prepare("SELECT extFlatId, flStatus, flPrice, UNIX_TIMESTAMP(snapDate) FROM `snapshots` WHERE bId=? ORDER BY `snapId`;"))) {
+		echo "<p>Error: expStmt prepare failed: (" . $expStmt->errno . ") " . $expStmt->error ."<p>\r\n"; }
+	foreach ($buildings as $building){
+		if(!$expStmt->bind_param('i', $building[0])) {	
+			echo "<p>Error: expStmt binding parameters failed: (" . $expStmt->errno . ") " . $expStmt->error ."</p>\r\n"; }
+		if(!$expStmt->execute()) { echo "<p>Error: expStmt execute failed: (" . $expStmt->errno . ") " . $expStmt->error ."</p>\r\n";
+			}
+		if (!($expVal = $expStmt->get_result())) {
+			echo "<p>Error: expStmt getting result set failed: (" . $expStmt->errno . ") " . $expStmt->error ."</p>\r\n"; }
+		$expVal = $expVal->fetch_all();
+		//echo "<pre>"; print_r($expVal); echo "</pre>";
+		file_put_contents('./jsdb/bd'.$building[0].'_full.json',json_encode($expVal));
+		file_put_contents('compress.zlib://./jsdb/bd'.$building[0].'_full.json.gz',json_encode($expVal));
+		}
+	}
+
+$db->close();
+$time_end = microtime(true);
+echo "<p class='execTime'>Script execution time: ".round($time_end - $time_start, 2)." s.</p>\r\n";
+saveLog(ob_get_contents());
+$output = ob_get_contents();
+$ifErrors = stripos($output , 'error');
+if($ifErrors !== false || ($currHour == 20 && $currMinute >= 30 && $currMinute < 45)){
+	mb_language('UTF-8'); mb_internal_encoding('UTF-8');
+	$subject = 'New buildings appartments parser report';
+	$headers = "From: Icode Alerting Service<alert@icode.ru>\r\nMime-Version: 1.0\r\nContent-Type: text/html;charset=UTF-8\r\n";
+	if($ifErrors !== false) {
+		$subject .= ' with errors';
+		$headers .= "X-Priority: 1 (Highest)\r\nX-MSMail-Priority: High\r\nImportance: High";
+		}
+	mb_send_mail('support@icode.ru', $subject, '<html><body>'.$output.'</body></html>', $headers);
+	};
+
+ob_end_flush(); 
+//$time_end = microtime(true);
+//echo '<div class="scriptExecTime">execution time: '.round($time_end - $time_start, 2).' s.</div>';
+?></body></html>
--- test.php
+++ test.php
@@ -15,15 +15,27 @@ error_reporting(E_NOTICE);
 $time_start = microtime(true); 
 //function compareFllatSnapsByextFlatId ($a, $b){ return $a[4] - $b[4]; }
 function compareFlatIds ($a, $b){ return $a[4] - $b[4]; }
-function compareFlatPrices ($a, $b) {
-	//if( $a[4] == $b[4]) { echo "<pre>price: ".$a[6]." и ".$b[6]."</pre><br>"; }
-	return strcmp(implode("-", $a), implode("-", $b) );
-	}
+function compareFlatPrices ($a, $b) { return strcmp(implode("-", $a), implode("-", $b) ); }
+function compareFlatsByIdAndBId($a, $b) {
+	if($a[2] > $b[2]){ $res = 1;}
+	else if ($a[2] < $b[2]) { $res = -1;}
+	else {
+		if ($a[4] > $b[4]) { $res = 1;}
+		else if ($a[4] < $b[4]) { $res = -1;}
+		else { $res = 0; }
+		}
+	return $res;
+	} 
 
 require ("./simple_html_dom.php");
 
 //flat status: 0 - never available, 1 - available(on Sale), 2 - fixed, 3 - sold out
-$sources = array ('./examples/k1example_05022014.htm', './examples/k1example_06022014.htm', './examples/k1example_07022014.htm', './examples/k1example_08022014.htm','./examples/k1example_10022014.htm','./examples/k1example_11022014.htm');
+$sources = array ('./examples/k1example_05022014.htm', 
+	'./examples/k1example_06022014.htm',
+	//'./examples/k1example_07022014.htm','./examples/k1example_08022014.htm','./examples/k1example_10022014.htm','./examples/k1example_11022014.htm', './examples/k1example_12022014.htm'
+	);
+
+//$sources = array ('./examples/k2example_05022014.htm', './examples/k2example_06022014.htm', './examples/k2example_07022014.htm', './examples/k2example_08022014.htm','./examples/k2example_10022014.htm','./examples/k2example_11022014.htm','./examples/k2example_12022014.htm');
 
 function parseRmk9($address){
 	$html = file_get_html($address);
@@ -38,7 +50,7 @@ function parseRmk9($address){
 				case 1710: $bId = 3; break;
 				//default:
 				}
-			//$tmp[2] = intval($bId);//building
+			$tmp[2] = rand(1, 3);//building
 			$tmp[4] = intval($matches[3]);//extId
 			//$tmp[5] = 1;//status 'onSale'/available
 			$tmp[6] = intval($matches[1]);//price
@@ -49,7 +61,7 @@ function parseRmk9($address){
 	return $flatsOnSale;
 	}
 
-$i = 0;$res = array();
+$i = 0; $res = array();
 foreach ($sources as $source){
 	$res[$i] = parseRmk9($source);
 	if($i === 0) { $i++; continue; }
@@ -58,25 +70,23 @@ foreach ($sources as $source){
 	$newFlatsOnSale = array_udiff($res[$i], $res[$i-1], 'compareFlatIds'); 
 	$flatsOnSaleFromPrevCheck = array_udiff($res[$i], $newFlatsOnSale, 'compareFlatIds'); 
 	$flatsPricesUpd = array_udiff($flatsOnSaleFromPrevCheck, $res[$i-1], 'compareFlatPrices');
+	$summ = array_merge($soldFlats, $newFlatsOnSale, $flatsPricesUpd);
 	echo "<pre>";
-	echo "ушли: <br>";
-	print_r($soldFlats);
-	echo "появились: <br>";
-	print_r($newFlatsOnSale);
-	echo "изменилась цена: <br>";
-	print_r($flatsPricesUpd);
+	usort($summ,'compareFlatsByIdAndBId');
+	print_r($summ);
+	//if(count($soldFlats)) { echo "ушли: <br>"; print_r($soldFlats); }
+	//if(count($newFlatsOnSale)) { echo "появились: <br>"; print_r($newFlatsOnSale); }
+	//if(count($flatsPricesUpd)) { echo "изменилась цена: <br>"; print_r($flatsPricesUpd); }
 	echo "</pre>";	
 	$i++;
 	}
-//sort
+
 
 //res 0 - select extId, price from db where bId AND status=1
 //usort($soldFlats, "compareFlatIds");
 //usort($newFlatsOnSale, "compareFlatIds");
-
 //print_r($res[0]);
 //print_r($res[1]);
-
 
 $time_end = microtime(true);
 echo '<div class="scriptExecTime">execution time: '.round($time_end - $time_start, 2).' s.</div>';
